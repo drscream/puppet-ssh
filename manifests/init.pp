cdc82c5a8e718374a123d15ffd7bbdbc62301c48
Thomas Merkel <tm@core.io>
2013-04-05 19:36:50 +0200

    add new templates and remove old readme

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,35 +1,22 @@
-## Class: ssh
-#
-# This class installs openssh.
-#
-# Parameters:
-#    -   $permitRootLogin = ('without-password'|'no'|'yes')
-#
-# Actions:  
-#	Ensures that the openssh package is installed and distributes a config file.  ``/etc/ssh/sshd_config``                            
-#
-# Requires:  
-#	Package["openssh"]  
-#
-class ssh($permitRootLogin='no', $port='22' ) {
+class ssh( $permitRootLogin='no', $port='10110', $passwordAuthentication='no')
+{
+	package {'openssh-server':
+		ensure => present
+	}
 
-  package {'openssh-server':
-    ensure => present
-  }
+	file {'/etc/ssh/sshd_config':
+		content => template('ssh/sshd_config.erb'),
+		mode    => '0400',
+		notify  => Service['sshd'],
+		require => Package['openssh-server'],
+	}
 
-  file {'/etc/ssh/sshd_config':
-    content => template('ssh/sshd_config.erb'),
-    mode    => '0400',
-    notify  => Service['sshd'],
-    require => Package['openssh-server'],
-  }
-
-  service {'sshd':
-    name	=> 'ssh',
-    ensure	=> running,
-    enable	=> true,
-    hasstatus	=> true,
-    hasrestart	=> true,
-    require	=> File['/etc/ssh/sshd_config'],
-  }
+	service {'sshd':
+		name       => 'ssh',
+		ensure     => running,
+		enable     => true,
+		hasstatus  => true,
+		hasrestart => true,
+		require	   => File['/etc/ssh/sshd_config'],
+	}
 }
